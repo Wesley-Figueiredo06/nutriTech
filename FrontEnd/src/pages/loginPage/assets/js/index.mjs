dcf37b76920d3c071c6f2f85cd24533ed636a603
Wesley Figueiredo <weslk1111@gmail.com>
2025-05-01 12:48:47 -0300

    Criação - Tela principal e ajustes

---
+++ FrontEnd/src/pages/loginPage/assets/js/index.mjs
@@ -0,0 +1,20 @@
+
+
+const form = document.getElementById("form");
+const userInput = document.getElementById("userInput");
+const passwordInput = document.getElementById("passwordInput");
+const btnSubmit = document.getElementById("btnSubmit");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (userInput.value === "1" && passwordInput.value === "1") {
+        form.submit(alert("Login efetuado!"));
+    } else {
+        alert("usuario e senha errados!")
+    }
+
+});
+
+
